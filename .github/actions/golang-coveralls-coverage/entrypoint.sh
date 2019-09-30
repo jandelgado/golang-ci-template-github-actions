a4053b5a72e2d574e8d891a71be984a9567b6df2
Jan Delgado <jdelgado@gmx.net>
2019-10-01 00:09:17 +0200

    cleanup

--- .github/actions/golang-coveralls-coverage/entrypoint.sh
+++ .github/actions/golang-coveralls-coverage/entrypoint.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 set -eu
+
 unset GOROOT
 unset GOPATH
 
-env
-echo "IN=$INPUT_INFILE OUT=$INPUT_OUTFILE GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
 cd $GITHUB_WORKSPACE
-ls -l
-
-echo "*******************************"
-
-ls -l /go/bin/covfmt
 /go/bin/covfmt -infile "$INPUT_INFILE" -outfile "$INPUT_OUTFILE"
-
-ls -l $INPUT_INFILE || true
-ls -l $INPUT_OUTFILE || true
-
-echo "****INFILE****"
-cat $INPUT_INFILE || true
-echo "****OUTFILE****"
-cat $INPUT_OUTFILE || true
