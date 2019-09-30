aa8833f05e1395b43b794535d9ef64d3806164db
Jan Delgado <jdelgado@gmx.net>
2019-09-30 19:19:17 +0200

    fix

--- .github/actions/golang-coveralls-coverage/entrypoint.sh
+++ .github/actions/golang-coveralls-coverage/entrypoint.sh
@@ -2,5 +2,12 @@
 set -eu
 
 cd $GITHUB_WORKSPACE
+echo /covfmt -infile "$INPUT_INFILE" -outfile "$INPUT_OUTFILE"
 /covfmt -infile "$INPUT_INFILE" -outfile "$INPUT_OUTFILE"
 
+ls -l $INPUT_INFILE || true
+ls -l $INPUT_OUTFILE || true
+
+cat $INPUT_INFILE || true
+echo "*******************************"
+cat $INPUT_OUTFILE || true
